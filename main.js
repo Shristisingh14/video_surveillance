7c305901e13438e46169071e8ad92f848be23b2e
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-12-17 16:38:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,9 +11,10 @@ function setup(){
      video.hide();
 }
 
+
 function start() {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting Onjects";
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 
 }
 
@@ -23,4 +24,30 @@ function modelLoaded() {
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function draw() {
+    image(video, 0, 0, 500, 400);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResults);
+        for( i = 0; i < objects.length; i++ ){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected : " + objects.length;
+            fill("#00FF00");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 2, objects[i].y-15);
+            noFill();
+            stroke("#00FF00");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+    }
+}
+function gotResults(error, results) {
+  if(error){
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
 }
